5afdea5cca10d4f4e698548f5a14d771a396c838
parabola2004 <chengz21@mails.tsinghua.edu.cn>
2024-11-18 10:36:48 +0800

    should use unsigned long long for u64

--- modules/base/public/types.h
+++ modules/base/public/types.h
@@ -2,7 +2,7 @@
 
 using ssize = long;
 
-using u64 = unsigned long;
-using u32 = unsigned int;
-using u16 = unsigned short;
-using u8 = unsigned char;
+using u8    = unsigned char;
+using u16   = unsigned short;
+using u32   = unsigned int;
+using u64   = unsigned long long;
