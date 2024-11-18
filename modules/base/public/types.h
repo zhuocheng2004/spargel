853543bb4b7d8ba7a2a5b109cd428f7712c05900
Tianjiao Nie <ivwumupy@gmail.com>
2024-11-18 12:38:59 +0800

    improve basic types

--- modules/base/public/types.h
+++ modules/base/public/types.h
@@ -1,8 +1,14 @@
 #pragma once
 
-using ssize = long;
+// signed size
+using ssize = long long;
+static_assert(sizeof(ssize) == 8);
 
-using u8    = unsigned char;
-using u16   = unsigned short;
-using u32   = unsigned int;
-using u64   = unsigned long long;
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+static_assert(sizeof(u8) == 1);
+static_assert(sizeof(u16) == 2);
+static_assert(sizeof(u32) == 4);
+static_assert(sizeof(u64) == 8);
