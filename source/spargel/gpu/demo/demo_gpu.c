ab2e3932bbd3d6883f688c47d210b41da1cefd3d
Tianjiao Nie <ivwumupy@gmail.com>
2024-12-07 23:35:31 +0800

    gpu: clean up demo

--- source/spargel/gpu/demo/demo_gpu.c
+++ source/spargel/gpu/demo/demo_gpu.c
@@ -23,6 +23,7 @@
 
 struct renderer {
     sgpu_device_id device;
+    sgpu_surface_id surface;
     sgpu_swapchain_id swapchain;
     sgpu_command_queue_id queue;
     sgpu_command_buffer_id cmdbuf;
@@ -64,131 +65,82 @@ static void render(struct renderer* r) {
     r->frame++;
 }
 
-int main() {
-    sui_init_platform();
+static int create(int backend, char const* title, struct renderer* r) {
+    sui_window_id window = sui_create_window(500, 500);
+    sui_window_set_title(window, title);
 
     int result;
 
+    sgpu_device_id device;
+    {
+        struct sgpu_device_descriptor desc = {
+            .backend = SGPU_BACKEND_VULKAN,
+            .platform = sui_platform_id(),
+        };
+        result = sgpu_create_default_device(&desc, &device);
+    }
+    if (result != 0) return 1;
+    sbase_log_info("device created");
+
+    sgpu_command_queue_id queue;
+    result = sgpu_create_command_queue(device, &queue);
+    if (result != 0) return 1;
+    sbase_log_info("command queue created");
+
+    sgpu_surface_id surface;
+    {
+        struct sgpu_surface_descriptor desc = {
+            .window = window,
+        };
+        result = sgpu_create_surface(device, &desc, &surface);
+    }
+    if (result != 0) return 1;
+    sbase_log_info("surface created");
+
+    sgpu_swapchain_id swapchain;
+    {
+        struct sgpu_swapchain_descriptor desc = {
+            .surface = surface,
+            .width = 500,
+            .height = 500,
+        };
+        result = sgpu_create_swapchain(device, &desc, &swapchain);
+    }
+    if (result != 0) return 1;
+    sbase_log_info("swapchain created");
+
+    sgpu_command_buffer_id cmdbuf;
+    {
+        struct sgpu_command_buffer_descriptor desc = {
+            .queue = queue,
+        };
+        result = sgpu_create_command_buffer(device, &desc, &cmdbuf);
+    }
+    if (result != 0) return 1;
+    sbase_log_info("command buffer created");
+
+    r->device = device;
+    r->queue = queue;
+    r->surface = surface;
+    r->swapchain = swapchain;
+    r->cmdbuf = cmdbuf;
+    r->frame = 0;
+
+    sui_window_set_render_callback(window, (void (*)(void*))render, r);
+
+    return 0;
+}
+
+int main() {
+    sui_init_platform();
+
 #if USE_VULKAN
-    sui_window_id window_vk = sui_create_window(500, 500);
-    sui_window_set_title(window_vk, "Spargel Demo - Vulkan");
-    sgpu_device_id device_vk;
-    {
-        struct sgpu_device_descriptor desc = {
-            .backend = SGPU_BACKEND_VULKAN,
-            .platform = sui_platform_id(),
-        };
-        result = sgpu_create_default_device(&desc, &device_vk);
-    }
-    if (result != 0) return 1;
-    sbase_log_info("device created");
-
-    sgpu_command_queue_id queue_vk;
-    result = sgpu_create_command_queue(device_vk, &queue_vk);
-    if (result != 0) return 1;
-    sbase_log_info("command queue created");
-
-    sgpu_surface_id surface_vk;
-    {
-        struct sgpu_surface_descriptor desc = {
-            .window = window_vk,
-        };
-        result = sgpu_create_surface(device_vk, &desc, &surface_vk);
-    }
-    if (result != 0) return 1;
-    sbase_log_info("surface created");
-
-    sgpu_swapchain_id swapchain_vk;
-    {
-        struct sgpu_swapchain_descriptor desc = {
-            .surface = surface_vk,
-            .width = 500,
-            .height = 500,
-        };
-        result = sgpu_create_swapchain(device_vk, &desc, &swapchain_vk);
-    }
-    if (result != 0) return 1;
-    sbase_log_info("swapchain created");
-
-    sgpu_command_buffer_id cmdbuf_vk;
-    {
-        struct sgpu_command_buffer_descriptor desc = {
-            .queue = queue_vk,
-        };
-        result = sgpu_create_command_buffer(device_vk, &desc, &cmdbuf_vk);
-    }
-    if (result != 0) return 1;
-    sbase_log_info("command buffer created");
-
-    struct renderer data_vk = {
-        .device = device_vk,
-        .swapchain = swapchain_vk,
-        .queue = queue_vk,
-        .cmdbuf = cmdbuf_vk,
-        .frame = 0,
-    };
-    sui_window_set_render_callback(window_vk, (void (*)(void*))render, &data_vk);
+    struct renderer r_vk;
+    if (create(SGPU_BACKEND_VULKAN, "Spargel Demo - Vulkan", &r_vk) != 0) return 1;
 #endif
-
 #if USE_METAL
-    sui_window_id window_mtl = sui_create_window(500, 500);
-    sui_window_set_title(window_mtl, "Spargel Demo - Metal");
-    sgpu_device_id device_mtl;
-    {
-        struct sgpu_device_descriptor desc = {
-            .backend = SGPU_BACKEND_VULKAN,
-            .platform = sui_platform_id(),
-        };
-        result = sgpu_create_default_device(&desc, &device_mtl);
-    }
-    if (result != 0) return 1;
-    sbase_log_info("device created");
-
-    sgpu_command_queue_id queue_mtl;
-    result = sgpu_create_command_queue(device_mtl, &queue_mtl);
-    if (result != 0) return 1;
-    sbase_log_info("command queue created");
-
-    sgpu_surface_id surface_mtl;
-    {
-        struct sgpu_surface_descriptor desc = {
-            .window = window_mtl,
-        };
-        result = sgpu_create_surface(device_mtl, &desc, &surface_mtl);
-    }
-    if (result != 0) return 1;
-    sbase_log_info("surface created");
-
-    sgpu_swapchain_id swapchain_mtl;
-    {
-        struct sgpu_swapchain_descriptor desc = {
-            .surface = surface_mtl,
-            .width = 500,
-            .height = 500,
-        };
-        result = sgpu_create_swapchain(device_mtl, &desc, &swapchain_mtl);
-    }
-    if (result != 0) return 1;
-    sbase_log_info("swapchain created");
-
-    sgpu_command_buffer_id cmdbuf_mtl;
-    {
-        struct sgpu_command_buffer_descriptor desc = {
-            .queue = queue_mtl,
-        };
-        result = sgpu_create_command_buffer(device_mtl, &desc, &cmdbuf_mtl);
-    }
-    if (result != 0) return 1;
-    sbase_log_info("command buffer created");
-
-    struct renderer data_mtl = {
-        .device = device_mtl,
-        .swapchain = swapchain_mtl,
-        .queue = queue_mtl,
-        .cmdbuf = cmdbuf_mtl,
-        .frame = 0,
-    };
-    sui_window_set_render_callback(window_mtl, (void (*)(void*))render, &data_mtl);
+    struct renderer r_mtl;
+    if (create(SGPU_BACKEND_VULKAN, "Spargel Demo - Metal", &r_mtl) != 0) return 1;
 #endif
 
     sui_platform_run();
